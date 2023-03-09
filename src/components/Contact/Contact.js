f45aa18d86a403aa812f9b5e4475e867c227ad4f
codri5 <97296166+codri5@users.noreply.github.com>
2023-03-09 13:23:41 +0200

    added contact buttons

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import './Contact.css';
+import { SlLocationPin } from "react-icons/sl";
+import { AiOutlinePhone } from "react-icons/ai";
+import { AiOutlineMail } from "react-icons/ai";
+import { AiFillGithub } from 'react-icons/ai';
+
 
 function Contact() {
     return (
         <>
         <section>
           <Container className='contact' id='contact'>
-            <Container className='contact-content'>
-              <Row>
-                <Col className='contact-header text-center'>
-                  <h1 className='contact-heading'>Contact</h1>
+            <Container className='contact-content' id='contact-content'>
+                 <Row>
+                <Col>
+                  <h3 className='contact-heading text-center'>Get in<span id='span'>Touch</span></h3>
+                </Col>
+                <hr></hr>
+                <Col className='contBtn'>
+                <Row>
+                <Button className='contactBtn' href='#' target='_blank'>
+                        <SlLocationPin /> Liverpool
+                </Button>
+                </Row>
+                <Row>
+                <Button className='contactBtn' href='#' target='_blank'>
+                        <AiOutlinePhone />+44 754 242 4360
+                </Button>
+                </Row>
+                <Row>
+                <Button className='contactBtn' href='#' target='_blank'>
+                        <AiOutlineMail /> codrinapal@gmail.com
+                </Button>
+                </Row>
+                <Row>
+                <Button className='contactBtn' href='https://github.com/codri5' target='_blank'>
+                        <AiFillGithub /> GitHub
+                </Button>
+                </Row>
+              
+                  <form></form>
                 </Col>
               </Row>
             </Container>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
 import Cards from './Cards';
 import './Projects.css';
+import readmeGenerator from '../../assets/card-images/readme-generator.png';
+import poolPosition from '../../assets/card-images/pool-position.png';
+import newsSearch from '../../assets/card-images/news-search.png';
+import workScheduler from '../../assets/card-images/work-day-scheduler.png';
+import jsQuiz from '../../assets/card-images/javascript-quiz.png';
+import passGenerator from '../../assets/card-images/password-generator.png';
+
 
 function Projects() {
     return (
@@ -11,13 +18,15 @@ function Projects() {
         <Container className='projects-content text-center'>
           <Row>
             <Col>
-              <Cards 
+              <Cards
+                image={readmeGenerator} 
                 title='README Generator'
                 github='https://github.com/codri5/readme-generator'
               />
             </Col>
             <Col>
               <Cards
+                image={poolPosition}
                 title='Pool Position'
                 github='https://github.com/codri5/pool-position'
                 website='https://codri5.github.io/pool-position'
@@ -25,6 +34,7 @@ function Projects() {
             </Col>
             <Col>
               <Cards
+                image={newsSearch}
                 title='News Search'
                 github='https://github.com/codri5/news-search'
                 website='https://codri5.github.io/news-search'
@@ -34,13 +44,15 @@ function Projects() {
           <Row>
             <Col>
               <Cards
-                title='Work Day Scheduler'
+                image={workScheduler}
+                title='Workday Scheduler'
                 github='https://github.com/codri5/work-day-scheduler'
                 website='https://codri5.github.io/work-day-scheduler'
               />
             </Col>
             <Col>
               <Cards
+                image={jsQuiz}
                 title='JavaScript Quiz'
                 github='https://github.com/codri5/javascript-quiz'
                 website='https://codri5.github.io/javascript-quiz'
@@ -48,6 +60,7 @@ function Projects() {
             </Col>
             <Col>
               <Cards
+                image={passGenerator}
                 title='Password Generator'
                 github='https://github.com/codri5/password-generator'
                 website='https://codri5.github.io/password-generator'
